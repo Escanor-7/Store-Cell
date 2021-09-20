a531acc4143e665ecc290aba9f98cabc39a71054
cessilva <cessilva@rd.com.br>
2021-09-19 21:01:57 -0300

    Initial project setup

---
+++ store-cell/src/componentes/buttonHome/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ButtonHome.styles.css';
+
+const ButtonHome = () => {
+
+    return (
+        <div>
+            <button className='buttonHome'>
+                Comprar
+                <i class="bi bi-cart"></i>
+            </button>
+        </div>
+    )
+}
+
+export default ButtonHome;
---
+++ store-cell/src/paginas/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ButtonHome from '../../componentes/buttonHome/index';
+import Header from '../../componentes/Header/index';
+const Home = () => {
+
+    return (
+        <>
+            <Header />
+            <ButtonHome />
+        </>
+    )
+}
+
+export default Home;
