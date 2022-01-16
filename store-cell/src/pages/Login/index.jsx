0d75ed392cf02136d9bfcb74846e98bd41558c3e
jeymes <87048226+jeymes@users.noreply.github.com>
2022-01-16 13:49:17 -0300

    setting up peephole

--- store-cell/src/pages/Login/index.jsx
+++ store-cell/src/pages/Login/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as S from './Login.styles.js';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+
+// Validaçoes Abaixo >
 
 const validatioLogin = yup.object().shape({
     email: yup.string().required('O campo email é obrigatorio').max(40, 'O email precisa ter menos de 40 caracteres'),
@@ -12,6 +14,19 @@ const validatioLogin = yup.object().shape({
 })
 
 const Login = () => {
+
+    // OlhoMagico Abaixo >
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [show, setShow] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setShow(!show)
+    }
+
+    // Validaçoes Abaixo >
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validatioLogin)
@@ -29,7 +44,9 @@ const Login = () => {
                         <i class="bi bi-envelope"></i>
                         <input
                             type="email"
-                            placeholder="exemplo@exemplo.com.br" {...register("email")} />
+                            placeholder="exemplo@exemplo.com.br" {...register("email")}
+                            value={email}
+                            onChange={e => setEmail(e.target.value)} />
                     </div>
                     <span>{errors.email?.message}</span>
                 </S.InputContainer>
@@ -39,16 +56,25 @@ const Login = () => {
                     <div>
                         <i class="bi bi-shield-lock"></i>
                         <input
-                            type={"password"}
-                            placeholder="senha..." {...register("senha")} />
+                            type={show ? "text" : "password"}
+                            placeholder="senha..." {...register("senha")}
+                            value={password}
+                            onChange={e => setPassword(e.target.value)} />
 
-                        <S.PasswordEye>
-                            <i class="bi bi-eye" />
-                        </S.PasswordEye>
+                        {show ? (
+                            <S.PasswordEye>
+                                <i class="bi bi-eye"
+                                    size={20}
+                                    onClick={handleClick} />
+                            </S.PasswordEye>
+                        ) : (
+                            <S.PasswordEye>
+                                <i class="bi bi-eye-slash"
+                                    size={20}
+                                    onClick={handleClick}></i>
+                            </S.PasswordEye>
+                        )}
 
-                        <S.PasswordEye>
-                            <i class="bi bi-eye-slash"></i>
-                        </S.PasswordEye>
                     </div>
                     <span>{errors.senha?.message}</span>
                 </S.InputContainer>
